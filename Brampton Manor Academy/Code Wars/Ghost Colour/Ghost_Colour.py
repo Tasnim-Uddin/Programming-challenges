2fbcb8f8b640d2813133b15d4c0c42885022f08e
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-09-10 16:22:01 +0100

    Delete Brampton Manor Academy/Code Wars directory

--- Brampton Manor Academy/Code Wars/Ghost Colour/Ghost_Colour.py
+++
@@ -1,9 +0,0 @@
-import random
-
-class Ghost():
-  def colour(self):
-    self.colour = random.choice(["white", "yellow", "purple", "red"])
-    return self.colour
-
-ghost = Ghost()
-print(ghost.colour())
