abe4763eb6bdeef7202fc693d4781d3b7f679c00
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-09-10 16:51:00 +0100

    Add files via upload

--- Brampton Manor Academy/Code Wars/Upper_Case.py
+++ Brampton Manor Academy/Code Wars/Upper_Case.py
@@ -1,4 +1,13 @@
 #https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
 
-def is_uppercase(inp):
-    return inp.isupper()== upper()
+def wordInput():
+    word = input("Enter: ")
+    return word
+
+def is_uppercase(word):
+    return word.upper()== word
+
+if __name__ == "__main__":
+    word = wordInput()
+    upper = is_uppercase(word)
+    print(upper)
