65cc0f0f377a5c4f77f4a787a5f454139decf878
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-09-10 12:09:14 +0100

    Add files via upload

--- Brampton Manor Academy/Challenge 1/testing.py
+++ Brampton Manor Academy/Challenge 1/testing.py
@@ -5,29 +5,19 @@ from Rod_Conversions import *
 class testConversions(unittest.TestCase):
     
     def test_meters(self):
-        rods = 10
-        meters = metersConvert(rods)
-        self.assertEqual(meters, 50.292)
+        self.assertEqual(metersConvert(10), 50.292)
         
     def test_feet(self):
-        rods = 50
-        feet = feetConvert(rods)
-        self.assertEqual(feet, 825.0)
+        self.assertEqual(feetConvert(50), 825.0)
 
     def test_miles(self):
-        rods = 6
-        miles = milesConvert(rods)
-        self.assertEqual(miles, 0.018750000000000003)
+        self.assertEqual(milesConvert(6), 0.018750000000000003)
         
     def test_furlongs(self):
-        rods = 5
-        furlongs = furlongsConvert(rods)
-        self.assertEqual(furlongs, 0.125)
+        self.assertEqual(furlongsConvert(5), 0.125)
 
     def test_walk(self):
-        rods = 70
-        minutes = walkConvert(rods)
-        self.assertEqual(minutes, 4.233879503789322)
+        self.assertEqual(walkConvert(70), 4.233879503789322)
 
 if __name__ == '__main__':
     unittest.main()
