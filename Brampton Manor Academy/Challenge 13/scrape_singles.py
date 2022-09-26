38038a6f627257d2e955b00f68e13a273de5b198
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-09-26 22:43:35 +0100

    Add files via upload

--- Brampton Manor Academy/Challenge 13/scrape_singles.py
+++ Brampton Manor Academy/Challenge 13/scrape_singles.py
@@ -9,9 +9,11 @@ def scrape(url):
     fp.close()
     return mystr
 
+
 def find_position(html):
     count = 0
     i = 0
+    position_list = []
     while count < 10:
         open_tr = html.find('<tr>', i)
         close_tr = html.find("</tr>", open_tr)
@@ -21,14 +23,18 @@ def find_position(html):
         pos_start = newstring.find(look_for_pos)
         pos_end = newstring.find('</', pos_start)
         position = newstring[pos_start+len(look_for_pos):pos_end]
-        
+        position_list.append(position)
+
         i = close_tr
         count += 1
-        return position
+        
+    return position_list
+
 
 def find_title(html):
     count = 0
     i = 0
+    title_list = []
     while count < 10:
         open_tr = html.find('<tr>', i)
         close_tr = html.find("</tr>", open_tr)
@@ -36,21 +42,25 @@ def find_title(html):
 
         look_for_string = '"/search/singles/'
         string_start = newstring.find(look_for_string)
-        string_end = newstring.find('</',string_start)
+        string_end = newstring.find('a>',string_start)
         string = newstring[string_start:string_end]
 
         look_for_title = '/">'
         title_start = string.find(look_for_title)
         title_end = string.find('</',title_start)
         title = string[title_start+len(look_for_title):title_end]
-        
+        title_list.append(title)
+                
         i = close_tr
         count += 1
-        return title
+        
+    return title_list
+
 
 def find_artist(html):
     count = 0
     i = 0
+    artist_list = []
     while count < 10:
         open_tr = html.find('<tr>', i)
         close_tr = html.find("</tr>", open_tr)
@@ -58,27 +68,32 @@ def find_artist(html):
 
         look_for_string = '"/artist/'
         string_start = newstring.find(look_for_string)
-        string_end = newstring.find('</',string_start)
+        string_end = newstring.find('a>',string_start)
         string = newstring[string_start:string_end]
 
         look_for_artist = '/">'
         artist_start = string.find(look_for_artist)
         artist_end = string.find('</',artist_start)
         artist = string[artist_start+len(look_for_artist):artist_end]
+        artist_list.append(artist)
         
         i = close_tr
         count += 1
-        return artist
+    
+    return artist_list
+
+def output_table(position,title,artist):
+    print(f'{"NUMBER":<16} {"SONG":<35} {"ARTIST":<16}')
+    for p, t, a in zip(position, title, artist):
+        print(f'{p:<16} {t:<35} {a:<16}')
 
 
 if __name__ == "__main__":
     html = scrape(url)
-
-    print(f'{"POSITION":<16} {"SONG":<35} {"ARTIST":<16}')
-
     position = find_position(html)
     title = find_title(html)
     artist = find_artist(html)
+    table = output_table(position,title,artist)
 
-    print(f'{position:<16} {title:<35} {artist:<16}')
 
+    
