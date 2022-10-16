01d43bc0e6ff9ca4942f762084cf7032244593cf
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-10-16 23:11:24 +0100

    Add files via upload

--- Brampton Manor Academy/Java/GameOfLife/src/TinyLife.java
+++ Brampton Manor Academy/Java/GameOfLife/src/TinyLife.java
@@ -3,22 +3,25 @@ public class TinyLife {
     public static void main(String[] args) throws Exception {
         Long world = Long.parseLong(args[0]);
         print(world);
+        world = setCell(world, 1, 1, true );
+        System.out.println(world);
     }
 
     public static boolean getCell(long world, int col, int row) {
-        boolean value = (col > 7) | (col < 0) | (row > 7) | (row < 0);
-        return value;
+        boolean grid_check = (col > 7) | (col < 0) | (row > 7) | (row < 0);
+        return grid_check;
     }
 
-    public static long set(long world, int col, int row, boolean value) {
-
+    public static long setCell(long world, int col, int row, boolean value) {
+        int place = (col * 7) + (col + row);
+        long grid = PackedLong.set(world, place, value);
+        return grid;
     }
-
 
     public static void print(long world) {
         System.out.println("-");
         for (int row = 0; row < 8; row++) {
-            for (int col = 0; col < 8; col ++) {
+            for (int col = 0; col < 8; col++) {
                 System.out.print(getCell(world, col, row) ? "#" : "_");
             }
             System.out.println();
@@ -34,7 +37,4 @@ public class TinyLife {
     public static long nextGeneration(long world){
     }
 
-
 }
-
-
