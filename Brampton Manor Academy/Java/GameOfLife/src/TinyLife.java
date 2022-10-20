2a935ae66bbda79ae8051d363723e33bbf00e14a
Tasnim Uddin Syed <112864578+Tasnim-Uddin@users.noreply.github.com>
2022-10-20 14:55:58 +0100

    Add files via upload

--- Brampton Manor Academy/Java/GameOfLife/src/TinyLife.java
+++ Brampton Manor Academy/Java/GameOfLife/src/TinyLife.java
@@ -1,10 +1,7 @@
 public class TinyLife {
 
     public static void main(String[] args) throws Exception {
-        Long world = Long.decode(args[0]);
-        print(world);
-        Long updated_world = setCell(world, 1, 1, true );
-        print(updated_world);
+        play(Long.decode(args[0]));
     }
 
     public static boolean getCell(long world, int col, int row) {
@@ -13,8 +10,8 @@ public class TinyLife {
     }
 
     public static long setCell(long world, int col, int row, boolean value) {
-        int place = (col * 7) + (col + row);
-        long updated_world = PackedLong.set(world, place, value);
+        int point = col + (row * 8);
+        long updated_world = PackedLong.set(world, point, value);
         return updated_world;
     }
 
@@ -29,4 +26,51 @@ public class TinyLife {
     }
 
 
+    public static int countNeighbours(long world, int col, int row) {
+        int neighbours = 0;
+        //the ? replaces the if, they mean the same thing except that using ? allows u to do it in 1 line
+        neighbours += (getCell(world, col-1, row-1)) ? 1: 0;
+        neighbours += (getCell(world, col, row-1)) ? 1: 0;
+        neighbours += (getCell(world, col+1, row-1)) ? 1: 0;
+        neighbours += (getCell(world, col-1, row)) ? 1: 0;
+        neighbours += (getCell(world, col+1, row)) ? 1: 0;
+        neighbours += (getCell(world, col-1, row+1)) ? 1: 0;
+        neighbours += (getCell(world, col, row+1)) ? 1: 0;
+        neighbours += (getCell(world, col+1, row+1)) ? 1: 0;
+        return neighbours;
+    }
+
+    public static boolean computeCell(long world,int col, int row){
+        boolean liveCell = getCell(world, col, row);
+        int neighbours = countNeighbours(world, col, row);
+        boolean nextCell = false;
+        if (neighbours < 2) {
+            nextCell = false;
+        }
+        if (liveCell && neighbours == 2 || liveCell && neighbours == 3) {
+            nextCell = true;
+        }
+        if (neighbours > 3) {
+            nextCell = false;
+        }
+        if (!liveCell && neighbours == 3) {
+            nextCell = true;
+        }
+        return nextCell;
+    }
+
+    public static long nextGeneration(long world){
+
+    }
+
+    public static void play(long world) throws Exception {
+        int userResponse = 0;
+        while (userResponse != 'q'){
+            print(world);
+            userResponse = System.in.read();
+            world = nextGeneration(world);
+        }
+    }
 }
+
+
